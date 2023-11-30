155735324c689004a396ce7794bcae309046fa52
Nadau07 <noah.dev7@gmail.com>
2023-11-30 23:19:17 +0100

    Header OK (revoir CSS mobile first)

--- src/components/general/Header.jsx
+++ src/components/general/Header.jsx
@@ -1,22 +1,57 @@
 import "./styles/header.css";
 import HeaderImg from "../../images/profil/maneki-neko.jpg";
-//import Competences from "./Competences";
+import iconGithub from "../../images/icons_reseaux/github.svg";
+import iconLinkedIn from "../../images/icons_reseaux/linkedin.svg";
+import RubberBand from "react-reveal/RubberBand";
+import Zoom from "react-reveal/Zoom";
+import { useState } from "react";
 
-function Header(){
+function Header() {
 
-    return(
-    
-            <div className="container_header">
-                <img className="container_header_image "src={HeaderImg} alt="imageheader" />
+    const [isGithubHovered, setGithubHovered] = useState(false);
+    const [isLinkedInHovered, setLinkedInHovered] = useState(false);
 
-                <div className="header_paragraphe">
-                    <h1>Noah Liebert</h1>
-                    <p>Intégrateur Web</p>
-                </div>
+  return (
+    <div className="container_header">
+      <img
+        className="container_header_image "
+        src={HeaderImg}
+        alt="imageheader"
+      />
+
+      <div className="header_paragraphe">
+        <Zoom top>
+          <h1>Noah Liebert</h1>
+        </Zoom>
+        <RubberBand>
+            <p>Intégrateur Web</p> 
+        </RubberBand>
+       
+        <div className="icons_reseaux">
+        <img
+            className={`icons ${isGithubHovered ? 'fa-beat' : ''}`}
+            src={iconGithub}
+            alt="icon_github"
+            onMouseEnter={() => setGithubHovered(true)}
+            onMouseLeave={() => setGithubHovered(false)}
+          />
+          <img
+            className={`icons ${isLinkedInHovered ? 'fa-beat' : ''}`}
+            src={iconLinkedIn}
+            alt="icon_linkedIn"
+            onMouseEnter={() => setLinkedInHovered(true)}
+            onMouseLeave={() => setLinkedInHovered(false)}
+          />
         </div>
-    
-    
-    )
+        
+      </div>
+      <div class="scroll-downs">
+  <div class="mousey">
+    <div class="scroller"></div>
+  </div>
+</div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
