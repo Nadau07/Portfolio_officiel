32d9acd62c9470896e7a7e6af18bf459d4272985
Nadau07 <noah.dev7@gmail.com>
2023-12-02 08:59:36 +0100

    Avancement projets slider

--- src/components/projets/Projets.jsx
+++ src/components/projets/Projets.jsx
@@ -1,45 +1,20 @@
 import "./styles/projets.css";
-import FicheProjet from "./FicheProjet";
-import { useState } from "react";
+//import FicheProjet from "./FicheProjet";
+
 
 function Projet(props) {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleVoirPlusClick = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
 
   return (
-    <div className="card">
-      <div className="side side--front">
+    <figure className="card">
+     
         <img src={props.cover} alt="imgCover" className="projet_image" />
-      </div>
-
-      <div className="side side--back">
-        
-        <div className="projet_info">
+        <figcaption>
+  <div className="projet_info">
           <div className="projet_info_title">{props.title}</div>
-          <div className="projet_info_description">{props.description}</div>
-          <div className="projet_info_tags">{props.tags}</div>
-          <button className="projet_info_button" onClick={handleVoirPlusClick}>
-            Voir plus
-          </button>
-        </div>
-      </div>
 
-      {showModal && (
-        <FicheProjet
-          onClose={handleCloseModal}
-          title={props.title}
-          description={props.description}
-          tags={props.tags}
-        />
-      )}
-    </div>
+        </div>
+        </figcaption>
+    </figure>
   );
 }
 
--- src/components/projets/Slider.jsx
+++ src/components/projets/Slider.jsx
@@ -1,12 +1,6 @@
 import "./styles/slider.css"
 import Projet from "./Projets";
 import {DataProjets} from "../../data/DataProjets";
-import {Swiper, SwiperSlide} from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { EffectCoverflow, Pagination, Navigation, EffectCards } from 'swiper/modules';
 
 /**
  *
@@ -14,51 +8,19 @@ import { EffectCoverflow, Pagination, Navigation, EffectCards } from 'swiper/mod
  *
  */
 
-
-
-
 function Slider(props) {
   return (
     <div className="slider-container">
-      <div className="heading">
-        <h2 className="slider-container_title">Mes réalisations:</h2>
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          loop={true}
-          slidesPerView={'auto'}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }}
-          pagination={{ clickable: true }}
-          navigation={true} // Active la navigation
-          className="swiper_container"
-        >
-          {DataProjets.map((projet) => (
-            <SwiperSlide key={projet.id}>
-              <Projet
-                title={projet.title}
-                cover={projet.cover}
-                description={projet.description}
-                tags={projet.tags}
-                id={projet.id}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        <div className="slider-controler">
-          {/* Les flèches de navigation automatiques générées par Swiper */}
-          <div className="swiper-button-prev slider-arrow"></div>
-          <div className="swiper-button-next slider-arrow"></div>
-          {/* La pagination générée par Swiper */}
-          <div className="swiper-pagination"></div>
-        </div>
-      </div>
+{DataProjets.map((projet) => (
+    <Projet
+      key={projet.id}
+      title={projet.title}
+      cover={projet.cover}
+      description={projet.description}
+      tags={projet.tags}
+      id={projet.id}   
+    />
+  ))}
     </div>
   );
 }
