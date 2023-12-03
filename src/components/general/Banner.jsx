9080c8bd8aff7a3f2d274ad6c99e11aa44d164f4
Nadau07 <noah.dev7@gmail.com>
2023-12-03 17:33:12 +0100

    composant menu en cours de realisation

--- src/components/general/Banner.jsx
+++ src/components/general/Banner.jsx
@@ -1,40 +1,22 @@
 import "./styles/banner.css";
-import iconNavBar from "../../images/utils/bars-solid.svg";
-import { useState } from 'react';
-
 
 function Banner() {
-    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <div className="navbar_container">
+      <div className="navbar_logo">
+        <p className="logo_p">NL</p>
+      </div>
 
-    const toggleMobileMenu = () => {
-      setMobileMenuOpen(!isMobileMenuOpen);
-    };
-    const mobileMenuClass = isMobileMenuOpen ? 'mobile-display' : '';
+      <div className="navbar_list">
+        <ul>
+          <li>Accueil</li>
+          <li>Profil</li>
+          <li>Réalisations</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
-
-    return (
-        <div className={`navbar ${mobileMenuClass}`}>
-          <p className="logo">PORTFOLIO</p>
-    
-          <div className="nav-links">
-            <ul>
-              <li className="active">Accueil</li>
-              <li>Profil</li>
-              <li>Parcours</li>
-              <li>Réalisations</li>
-              <li>Contact</li>
-            </ul>
-          </div>
-          <img
-      src={iconNavBar}
-      alt="menu"
-      className="menu-mobile"
-      onClick={() => {
-        toggleMobileMenu();
-      }}
-    />
-        </div>
-      );
-    }
-    
-    export default Banner;
+export default Banner;
