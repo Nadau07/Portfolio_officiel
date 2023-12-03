50649693250758bd58e91edbc7ec7e35cc0ce59b
Nadau07 <noah.dev7@gmail.com>
2023-12-03 18:54:27 +0100

    avancement sur les cards projets + footer

--- src/components/general/Footer.jsx
+++ src/components/general/Footer.jsx
@@ -4,7 +4,7 @@ import "./styles/footer.css"
 function Footer(){
     return(
         <div className="footer">
-
+                <p className="footer_p"> © 2023 Noah Liebert. Tous droits réservés. </p>
         </div>
     )
 }
